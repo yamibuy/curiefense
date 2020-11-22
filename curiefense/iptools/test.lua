d719873602fe40c004f684b0f7284d4f5cf97edb
phil <phil@secdev.org>
2020-11-22 18:55:52 +0100

    Moved from treeset to treemap to store comment with net range

--- curiefense/iptools/test.lua
+++ curiefense/iptools/test.lua
@@ -3,20 +3,30 @@ ipt = require("iptools")
 
 s = ipt.new_ip_set()
 
-print("contains 1.2.3.4:",s:contains("1.2.3.4"))
-print("add 1.2.3.4")
-s:add("1.2.3.4")
-print("contains 1.2.3.4:",s:contains("1.2.3.4"))
-print("contains 1.2.3.5:",s:contains("1.2.3.5"))
+nets = {
+   ["1.2.3.4"] = "this is ip 1.2.3.4",
+   ["192.168.10.0/24"] = "this is net 192.168.10.0/24",
+   ["192.168.15.0/24"] = "this is net 192.168.15.0/24"
+}
 
-s:add("192.168.10.0/24")
-s:add("192.168.15.0/24")
+for k,v in pairs(nets) do
+   print("Adding ", k, "  ", v)
+   s:add(k,v)
+end
 
-print("should be true: ", s:contains("192.168.10.0/23"))
-print("should be true : ", s:contains("192.168.10.0/24"))
-print("should be true : ", s:contains("192.168.10.0/25"))
-print("should be false: ", s:contains("192.168.11.0"))
-print("should be true : ", s:contains("192.168.10.0"))
-print("should be true : ", s:contains("192.168.10.5"))
-print("should be true : ", s:contains("192.168.15.5"))
-print("size of set:", s:len())
+tests = {
+   ["1.2.3.4"] = true,
+   ["1.2.3.5"] = false,
+   ["192.168.10.0/24"] = true,
+   ["192.168.11.0/24"] = false,
+   ["192.168.15.0/24"] = true,
+   ["192.168.10.0/23"] = true,
+   ["192.168.10.0/25"] = true,
+}
+
+for k,v in pairs(tests) do
+   print("get ", k, "=>", s:get(k))
+   if s:contains(k) == v then print("ok") else print("KO") end
+end
+
+
