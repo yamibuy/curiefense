ad0d322973ea8a8ed080a08d9f55d3ecb1ee5649
tby <t@reblaze.com>
2020-11-27 13:57:27 +0200

    section.entries...

--- curiefense/curieproxy/lua/tagprofiler.lua
+++ curiefense/curieproxy/lua/tagprofiler.lua
@@ -74,29 +74,30 @@ end
 
 function eval_section(request_map, section)
   local section_relation_and = (section.relation == "AND")
+  local section_entries = section.entries
   local m_singles, m_pairs, m_iprange, nm_singles, nm_pairs, nm_iprange  = true, true, true, true, true, true
 
 
-  if section.entries.singles then
-    request_map.handle:logDebug(string.format("match_or_list section.singles %s", json_encode(section.entries.singles)))
-    local annotation, tags = match_singles(request_map, section.entries.singles)
+  if section_entries.singles then
+    request_map.handle:logDebug(string.format("match_or_list section.singles %s", json_encode(section_entries.singles)))
+    local annotation, tags = match_singles(request_map, section_entries.singles)
     if not annotation then
       m_singles = false
       if section_relation_and then return false end
     end
   end
 
-  if section.entries.pairs then
-    request_map.handle:logDebug(string.format("match_or_list section.entries.pairs %s", json_encode(section.entries.pairs)))
-    local annotation, tags = match_pairs(request_map, section.entries.pairs)
+  if section_entries.pairs then
+    request_map.handle:logDebug(string.format("match_or_list section_entries.pairs %s", json_encode(section_entries.pairs)))
+    local annotation, tags = match_pairs(request_map, section_entries.pairs)
     if not annotation then
       m_pairs = false
       if section_relation_and then return false end
     end
   end
 
-  if section.entries.iprange then
-    local within = section.entries.iprange:contains(request_map.attrs.ip)
+  if section_entries.iprange then
+    local within = section_entries.iprange:contains(request_map.attrs.ip)
     if not within then
       m_iprange = false
       if section_relation_and then return false end
@@ -104,24 +105,24 @@ function eval_section(request_map, section)
   end
 
   -- has entries
-  if section.entries.negate_singles and next(section.entries.negate_singles) then
-    local annotation, tags = match_singles(request_map, section.entries.negate_singles)
+  if section_entries.negate_singles and next(section_entries.negate_singles) then
+    local annotation, tags = match_singles(request_map, section_entries.negate_singles)
     if annotation then
       nm_singles = false
       if section_relation_and then return false end
     end
   end
 
-  if section.entries.negate_pairs and next(section.entries.negate_pairs) then
-    local annotation, tags = negate_match_pairs(request_map, section.entries.negate_pairs)
+  if section_entries.negate_pairs and next(section_entries.negate_pairs) then
+    local annotation, tags = negate_match_pairs(request_map, section_entries.negate_pairs)
     if not annotation then
       nm_pairs = false
       if section_relation_and then return false end
     end
   end
 
-  if section.entries.negate_iprange:len() > 0 then
-    local within = section.entries.negate_iprange:contains(request_map.attrs.ip)
+  if section_entries.negate_iprange:len() > 0 then
+    local within = section_entries.negate_iprange:contains(request_map.attrs.ip)
     if within then
       nm_iprange = false
       if section_relation_and then return false end
