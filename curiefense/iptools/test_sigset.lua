4ad586635f588ed18e8e88d7e19802553ca4f359
phil <phil@secdev.org>
2020-12-06 10:10:34 +0100

    Added test_sigset.lua

---
+++ curiefense/iptools/test_sigset.lua
@@ -0,0 +1,36 @@
+ipt = require("iptools")
+
+ss = ipt.new_sig_set()
+
+ss:add("^A+$","only As")
+ss:add("^B+$","only Bs")
+ss:add("^C+$","only Cs")
+ss:add("^A+","starts with A")
+ss:add("B+$","ends with B")
+ss:compile()
+
+
+tests = { "AAAAAAA", "BBBBBB", "AAABBB", "CABC", "ABABAB", "qADAZD" }
+
+print("======= test is_match() ========")
+
+for i,t in pairs(tests) do
+   print(t, ss:is_match(t))
+end
+
+print("======= test is_match_id() ========")
+
+for i,t in pairs(tests) do
+   print(t, ss:is_match_id(t))
+end
+
+
+print("======= test is_match_ids() ========")
+
+for i,t in pairs(tests) do
+   r = ss:is_match_ids(t)
+   print("--->", t)
+   for j,u in pairs(r) do
+      print("    ",u)
+   end
+end
