2b129366b4d14b42c33f52780237ab4b69668054
tby <t@reblaze.com>
2020-12-07 14:53:55 +0200

    :waf debug -- removed the inner loop

--- curiefense/curieproxy/lua/waf.lua
+++ curiefense/curieproxy/lua/waf.lua
@@ -203,6 +203,9 @@ function check(waf_profile, request)
                         request.handle:logInfo(string.format("WAFRustSignatures MATCHED -- iter over %s", msig))
                         if not section_exclude_ids[msig] then
                             if globals.WAFSignatures then
+                                for k, v in pairs(globals.WAFSignatures) do
+                                    request.handle:logDebug(string.format("globals.WAFSignatures -- Sig %s: %s", k, v))
+                                end
                                 local waf_sig = globals.WAFSignatures[msid]
                                 request.handle:logInfo(string.format("WAF block by Sig %s", waf_sig.id))
                                 return WAFBlock, gen_block_info(section, name, value, waf_sig)
