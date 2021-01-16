95fd31e0bde382a31f75fd204a7c32a55ec68b2a
Xavier <xavier@reblaze.com>
2021-01-16 23:01:01 +0100

    elasticsearch: improve initial config (create lifecycle policy, mapping, index)

--- curiefense/images/curielogger/init/start_curielogger.sh
+++ curiefense/images/curielogger/init/start_curielogger.sh
@@ -1,31 +1,71 @@
 #!/bin/bash
 
-create_index () {
-	PATTERN_PATH="api/saved_objects/index-pattern/curiefense"
-	INDEX_NAME=${CURIEFENSE_ES_INDEX_NAME:-curieaccesslog}
+ES_PATTERN_PATH="api/saved_objects/index-pattern/curiefense"
+ES_INDEX_NAME=${CURIEFENSE_ES_INDEX_NAME:-curieaccesslog}
+CURLJS="curl --silent -H 'Content-Type: application/json'"
+CURLCODE="curl --silent --write-out '%{http_code}\n' --output /dev/null"
+
+wait_for_es () {
+	curl --silent --output /dev/null -X GET "${ELASTICSEARCH_URL}_cluster/health?wait_for_status=yellow&timeout=60s"
+}
+
+define_es_lifecycle_policy () {
+	if $CURLCODE "${ELASTICSEARCH_URL}_ilm/policy/curie_policy"|grep -qv 404; then
+		echo "Elasticsearch lifecycle policy already exists."
+	else
+		$CURLJS -X PUT "${ELASTICSEARCH_URL}_ilm/policy/curie_policy" --data-binary @/init/es_lifecycle_policy.json
+		echo "Elasticsearch lifecycle policy defined."
+	fi
+}
+
+define_es_index () {
+	if $CURLCODE "$ELASTICSEARCH_URL$ES_INDEX_NAME-000001"|grep -qv 404; then
+		echo "Elastic index and alias already exists."
+	else
+		sed "s/INDEX_NAME/$ES_INDEX_NAME/" /init/es_index.json|$CURLJS -X PUT -d @- "$ELASTICSEARCH_URL$ES_INDEX_NAME-000001"
+		echo "Elastic index and alias created."
+	fi
+}
+
+define_es_mapping () {
+	if $CURLCODE "$ELASTICSEARCH_URL$ES_INDEX_NAME"|grep -qv 404; then
+		echo "Elasticsearch mapping already exists."
+	else
+		$CURLJS -XPUT "$ELASTICSEARCH_URL$ES_INDEX_NAME" --data-binary @/init/type_mapping.json
+		echo "Elasticsearch mapping created."
+	fi
+}
+
+create_kibana_index_pattern () {
 	# Wait for kibana to become reachable
 	while true; do
-		if curl --write-out "%{http_code}\n" --output /dev/null --silent "${CURIELOGGER_KIBANA_URL}api/status"|grep -q 200; then
+		if $CURLCODE "${CURIELOGGER_KIBANA_URL}api/status"|grep -q 200; then
 			break
 		fi
+		echo "Kibana at $CURIELOGGER_KIBANA_URL is not reachable yet, waiting 5s..."
 		sleep 5
-		RETRIES=$((RETRIES+1))
 	done
 
 	# Check whether the index pattern already exists
-	if curl  --write-out '%{http_code}\n' --output /dev/null --silent "${CURIELOGGER_KIBANA_URL}$PATTERN_PATH"|grep -qv 404; then
+	if $CURLCODE "$CURIELOGGER_KIBANA_URL$ES_PATTERN_PATH"|grep -qv 404; then
 		# already exists
-		exit 0
+		echo "Kibana index already exists."
+	else
+		# Create the index pattern
+		$CURLJS -X POST "$CURIELOGGER_KIBANA_URL$ES_PATTERN_PATH" -H 'kbn-xsrf: true' -d '{"attributes": {"title": "'"$ES_INDEX_NAME"'","timeFieldName": "@timestamp"}}'
+		echo "Kibana index $ES_INDEX_NAME created."
 	fi
-
-	# Create the index pattern
-	curl --silent -X POST "${CURIELOGGER_KIBANA_URL}$PATTERN_PATH" -H 'Content-Type: application/json' -H 'kbn-xsrf: true' -d '{"attributes": {"title": "'"$INDEX_NAME"'","timeFieldName": "@timestamp"}}'
 }
+
 
 if [ -n "$CURIELOGGER_INITIALIZE_KIBANA_INDEX" ]; then
 	# in case logs are saved in elasticsearch and not postgres
 	>&2 echo "Creating an index pattern in Kibana if needed."
-	create_index
+	wait_for_es
+	define_es_lifecycle_policy
+	define_es_index
+	define_es_mapping
+	create_kibana_index_pattern
 fi
 
 /bin/curielogger
