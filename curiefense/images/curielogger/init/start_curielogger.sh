76c44af8fc300ee202cf37498d84de759cccb7dc
Xavier <xavier@reblaze.com>
2021-01-17 18:29:03 +0100

    elasticsearch: further deployment fixes

--- curiefense/images/curielogger/init/start_curielogger.sh
+++ curiefense/images/curielogger/init/start_curielogger.sh
@@ -2,44 +2,47 @@
 
 ES_PATTERN_PATH="api/saved_objects/index-pattern/curiefense"
 ES_INDEX_NAME=${CURIEFENSE_ES_INDEX_NAME:-curieaccesslog}
-CURLJS="curl --silent -H Content-Type:application/json --output /dev/null"
-CURLCODE="curl --silent --write-out %{http_code}\n --output /dev/null"
+CURL="curl --silent --write-out %{http_code}\n -H Content-Type:application/json --output /dev/null"
 
 wait_for_es () {
-	curl --silent --output /dev/null -X GET "${ELASTICSEARCH_URL}_cluster/health?wait_for_status=yellow&timeout=60s"
+	if $CURL -X GET "${ELASTICSEARCH_URL}_cluster/health?wait_for_status=yellow&timeout=60s"|grep -qv 200; then
+		sleep 5
+		wait_for_es
+	fi
 }
 
 define_es_lifecycle_policy () {
-	if $CURLCODE "${ELASTICSEARCH_URL}_ilm/policy/curie_policy"|grep -qv 404; then
+	if $CURL "${ELASTICSEARCH_URL}_ilm/policy/curie_policy"|grep -q 200; then
 		echo "Elasticsearch lifecycle policy already exists."
 	else
-		$CURLJS -X PUT "${ELASTICSEARCH_URL}_ilm/policy/curie_policy" --data-binary @/init/es_lifecycle_policy.json
-		echo "Elasticsearch lifecycle policy defined."
+		if $CURL -X PUT "${ELASTICSEARCH_URL}_ilm/policy/curie_policy" --data-binary @/init/es_lifecycle_policy.json|grep -q 200; then
+			echo "Elasticsearch lifecycle policy defined."
+		else
+			echo "Elasticsearch lifecycle policy creation failed, retrying."
+			sleep 5
+			define_es_lifecycle_policy
+		fi
 	fi
 }
 
-define_es_index () {
-	if $CURLCODE "$ELASTICSEARCH_URL$ES_INDEX_NAME-000001"|grep -qv 404; then
-		echo "Elastic index and alias already exists."
+define_es_index_mapping () {
+	if $CURL "$ELASTICSEARCH_URL$ES_INDEX_NAME-000001"|grep -q 200; then
+		echo "Elastic index already exists."
 	else
-		sed "s/INDEX_NAME/$ES_INDEX_NAME/" /init/es_index.json|$CURLJS -X PUT -d @- "$ELASTICSEARCH_URL$ES_INDEX_NAME-000001"
-		echo "Elastic index and alias created."
-	fi
-}
-
-define_es_mapping () {
-	if $CURLCODE "$ELASTICSEARCH_URL$ES_INDEX_NAME"|grep -qv 404; then
-		echo "Elasticsearch mapping already exists."
-	else
-		$CURLJS -XPUT "$ELASTICSEARCH_URL$ES_INDEX_NAME" --data-binary @/init/type_mapping.json
-		echo "Elasticsearch mapping created."
+		if sed "s/INDEX_NAME/$ES_INDEX_NAME/" /init/es_index.json|$CURL -X PUT -d @- "$ELASTICSEARCH_URL$ES_INDEX_NAME-000001"|grep -q 200; then
+			echo "Elastic index and alias created."
+		else
+			echo "Elastic index and alias creation failed, retrying."
+			sleep 5
+			define_es_index_mapping
+		fi
 	fi
 }
 
 create_kibana_index_pattern () {
 	# Wait for kibana to become reachable
 	while true; do
-		if $CURLCODE "${CURIELOGGER_KIBANA_URL}api/status"|grep -q 200; then
+		if $CURL "${CURIELOGGER_KIBANA_URL}api/status"|grep -q 200; then
 			break
 		fi
 		echo "Kibana at $CURIELOGGER_KIBANA_URL is not reachable yet, waiting 5s..."
@@ -47,13 +50,19 @@ create_kibana_index_pattern () {
 	done
 
 	# Check whether the index pattern already exists
-	if $CURLCODE "$CURIELOGGER_KIBANA_URL$ES_PATTERN_PATH"|grep -qv 404; then
+	if $CURL "$CURIELOGGER_KIBANA_URL$ES_PATTERN_PATH"|grep -q 200; then
 		# already exists
 		echo "Kibana index already exists."
 	else
 		# Create the index pattern
-		$CURLJS -X POST "$CURIELOGGER_KIBANA_URL$ES_PATTERN_PATH" -H 'kbn-xsrf: true' -d '{"attributes": {"title": "'"$ES_INDEX_NAME"'","timeFieldName": "@timestamp"}}'
-		echo "Kibana index $ES_INDEX_NAME created."
+		if $CURL -X POST "$CURIELOGGER_KIBANA_URL$ES_PATTERN_PATH" -H 'kbn-xsrf: true' -d '{"attributes": {"title": "'"$ES_INDEX_NAME"'","timeFieldName": "@timestamp"}}'|grep -q 200; then
+
+			echo "Kibana index $ES_INDEX_NAME created."
+		else
+			echo "Kibana index $ES_INDEX_NAME creation failed, retrying."
+			sleep 5
+			create_kibana_index_pattern
+		fi
 	fi
 }
 
@@ -63,8 +72,7 @@ if [ -n "$CURIELOGGER_INITIALIZE_KIBANA_INDEX" ]; then
 	>&2 echo "Creating an index pattern in Kibana if needed."
 	wait_for_es
 	define_es_lifecycle_policy
-	define_es_index
-	define_es_mapping
+	define_es_index_mapping
 	create_kibana_index_pattern
 fi
 
