43f57f5f140f76f68c9d161aa549e5cf5bc21894
Flavio Percoco <flaper87@gmail.com>
2021-01-21 14:55:56 +0000

    Allow to use datastreams for newer ES versions

--- curiefense/images/curielogger/init/start_curielogger.sh
+++ curiefense/images/curielogger/init/start_curielogger.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+INIT_DIR=${INIT_DIR:-/init}
 ES_PATTERN_PATH="api/saved_objects/index-pattern/curiefense"
 ES_INDEX_NAME=${CURIEFENSE_ES_INDEX_NAME:-curieaccesslog}
+USE_DATA_STREAMS=${USE_DATA_STREAM:-true}
+
 CURL="curl --silent --write-out %{http_code}\n -H Content-Type:application/json --output /dev/null"
 
 wait_for_es () {
 	if $CURL -X GET "${ELASTICSEARCH_URL}_cluster/health?wait_for_status=yellow&timeout=10s"|grep -qv 200; then
 		sleep 5
+        echo $ELASTICSEARCH_URL
 		wait_for_es
 	fi
 }
@@ -15,7 +19,7 @@ define_es_lifecycle_policy () {
 	if $CURL "${ELASTICSEARCH_URL}_ilm/policy/curie_policy"|grep -q 200; then
 		echo "Elasticsearch lifecycle policy already exists."
 	else
-		if $CURL -X PUT "${ELASTICSEARCH_URL}_ilm/policy/curie_policy" --data-binary @/init/es_lifecycle_policy.json|grep -q 200; then
+		if $CURL -X PUT "${ELASTICSEARCH_URL}_ilm/policy/$ES_INDEX_NAME" --data-binary @$INIT_DIR/es_lifecycle_policy.json|grep -q 200; then
 			echo "Elasticsearch lifecycle policy defined."
 		else
 			echo "Elasticsearch lifecycle policy creation failed, retrying."
@@ -25,11 +29,35 @@ define_es_lifecycle_policy () {
 	fi
 }
 
-define_es_index_mapping () {
+define_es_index_template() {
+	if $CURL "${ELASTICSEARCH_URL}_index_template/$ES_INDEX_NAME"|grep -q 200; then
+		echo "Elastic index template already exists."
+	else
+        if [[ "$USE_DATA_STREAMS" == "true" ]];
+        then
+            DATA_STREAMS='"data_stream": {},'
+        fi
+
+		if sed -e "s/INDEX_NAME/$ES_INDEX_NAME/" -e "s/DATA_STREAMS/$DATA_STREAMS/" $INIT_DIR/index_template.json|$CURL -X PUT -d @- "${ELASTICSEARCH_URL}_index_template/$ES_INDEX_NAME"|grep -q 200; then
+			echo "Elastic index template created"
+		else
+			echo "Elastic index template creation failed, retrying."
+			sleep 5
+			define_es_index_template
+		fi
+	fi
+
+}
+
+define_es_initial_index () {
+    if [[ "$USE_DATA_STREAMS" == "true" ]]; then
+		echo "Using datastreams, no need for initial index."
+        return
+    fi
 	if $CURL "$ELASTICSEARCH_URL$ES_INDEX_NAME-000001"|grep -q 200; then
 		echo "Elastic index already exists."
 	else
-		if sed "s/INDEX_NAME/$ES_INDEX_NAME/" /init/es_index.json|$CURL -X PUT -d @- "$ELASTICSEARCH_URL$ES_INDEX_NAME-000001"|grep -q 200; then
+		if sed "s/INDEX_NAME/$ES_INDEX_NAME/" $INIT_DIR/es_index.json|$CURL -X PUT -d @- "$ELASTICSEARCH_URL$ES_INDEX_NAME-000001"|grep -q 200; then
 			echo "Elastic index and alias created."
 		else
 			echo "Elastic index and alias creation failed, retrying."
@@ -72,7 +100,8 @@ if [ -n "$CURIELOGGER_INITIALIZE_KIBANA_INDEX" ]; then
 	>&2 echo "Creating an index pattern in Kibana if needed."
 	wait_for_es
 	define_es_lifecycle_policy
-	define_es_index_mapping
+	define_es_index_template
+	define_es_initial_index
 	create_kibana_index_pattern
 fi
 
