9c10331e95dabddcfd18b27091d815a2071679b4
Xavier <xavier@reblaze.com>
2021-02-01 18:48:47 +0100

    helm: do not run curielogserver if elastichsearch is used for logs
    Signed-off-by: Xavier <xavier@reblaze.com>

--- curiefense/images/uiserver/init/start_nginx.sh
+++ curiefense/images/uiserver/init/start_nginx.sh
@@ -6,4 +6,10 @@ fi
 if [ -f /run/secrets/uisslcrt/uisslcrt ]; then
 	sed -i 's/# TLS-K8S //' /init/nginx.conf
 fi
+
+if [ -n "$CURIELOGSERVER_DISABLED" ]; then
+	cp /init/http.es.conf /init/http.conf
+	cp /init/tls-k8s.es.conf /init/tls-k8s.conf
+fi
+
 /usr/sbin/nginx -g 'daemon off;'
