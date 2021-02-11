312636956074ada85e6c92e424cfd2c3f1a95466
tby <t@reblaze.com>
2021-02-11 07:23:46 +0200

    converstion from bq and metrics

--- curiefense/images/curielogger/elasticsearch/utils/convert.py
+++ curiefense/images/curielogger/elasticsearch/utils/convert.py
@@ -8,6 +8,74 @@ import fileinput
 import json
 import sys
 import datetime
+import argparse
+import socket
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--spoof", default=False, action="store_true", help="spoof private data")
+
+from metrics import process_metrics
+
+atbash={"a":"z","A":"Z","b":"y","B":"Y","c":"x","C":"X","d":"w","D":"W","e":"v","E":"V","f":"u","F":"U","g":"t","G":"T","h":"s","H":"S","i":"r","I":"R","j":"q","J":"Q","k":"p","K":"P","l":"o","L":"O","M":"N","m":"n","N":"M","n":"m","o":"l","O":"L","p":"k","P":"K","q":"j","Q":"J","r":"i","R":"I","s":"h","S":"H","t":"g","T":"G","u":"f","U":"F","v":"e","V":"E","w":"d","W":"D","x":"c","X":"C","y":"b","Y":"B","z":"a","Z":"A"}
+atbash["0"]="9"
+atbash["1"]="8"
+atbash["2"]="7"
+atbash["3"]="6"
+atbash["4"]="5"
+atbash["5"]="4"
+atbash["6"]="3"
+atbash["7"]="2"
+atbash["8"]="1"
+atbash["9"]="0"
+
+def spoof_text(_input):
+    try:
+        output = (atbash.get(c, c) for c in _input)
+        return "".join(output).encode("rot13")
+    except:
+        return _input
+
+def sppof_ip(ipaddr):
+    try:
+        ipnum = struct.unpack("!I", socket.inet_aton(ipaddr))[0]
+        ipnum = ipnum ^ 24
+        return socket.inet_ntoa(struct.pack("!I", ipnum))
+    except:
+        return ipaddr
+
+def spoof_record(record):
+    record["authority"] = sppof_text(record["authority"])
+    record["path"] = sppof_text(record["path"])
+
+    spoofed_client_ip = spoof_ip(record["downstream"]["remoteaddress"])
+    spoofed_origin_ip = spoof_ip(record["upstream"]["remoteaddress"])
+
+    record["downstream"]["remoteaddress"] = spoofed_client_ip
+    record["upstream"]["remoteaddress"] = spoofed_origin_ip
+    # record["headers"]["host"] = sppof_text(record["headers"]["host"])
+
+    record["request"]["originalpath"] = sppof_text(record["request"]["originalpath"])
+    # xff = request["headers"].get("x-forwarded-for")
+    # if xff:
+    #     request["headers"]["x-forwarded-for"] = ", ".join((sppof_ip(i) for i in xff.split(", ")))
+
+    for n, v in request.get("cookies", {}).items():
+        request["cookies"][n] = spoof_text(v)
+
+    for n, v in request.get("arguments", {}).items():
+        request["arguments"][n] = spoof_text(v)
+
+    for n, v in request.get("headers", {}).items():
+        request["headers"][n] = spoof_text(v)
+
+    tags = [t for t in record["tags"] if not t.startswith("ip-")]
+    tags.append("ip-%s" % spoofed_client_ip.replace(".", "-"))
+    record["tags"] = tags
+
+    return record
+
 
 def dictify_headers(request):
     block = request["request_headers"]
@@ -46,14 +114,13 @@ def tagify (tags, record):
     return tags
 
 
-def reblazer_to_elastic(json_line):
-    ngxjson = json.loads(json_line)
+def reblazer_to_elastic(ngxjson, timestamp):
     anything_else = json.loads(ngxjson.get('anything_else', "{}"))
 
     eljson = {}
 
     eljson["requestid"] = ngxjson["request_id"]
-    eljson["@timestamp"] = datetime.datetime.fromtimestamp(float(ngxjson["timestamp"])).isoformat()
+    eljson["@timestamp"] = timestamp
 
     tls_used = len(ngxjson["ssl_protocol"]) > 2
     eljson["scheme"] = (tls_used and "https") or "http"
@@ -102,22 +169,37 @@ def reblazer_to_elastic(json_line):
         "code": int(ngxjson["status"])
     }
 
-    return eljson["requestid"], json.dumps(eljson)
+    # return spoof_record(eljson)
+    return eljson
 
+def gen_metric_index(recid):
+    return {"index" : {"_index" : "curiemetrics", "_id" : recid }}
+
+def gen_accesslog_index(recid):
+    return {"index" : {"_index" : "curieaccesslog", "_id" : recid }}
 
 def main():
 
     for line in fileinput.input():
         try:
-            recid, eljson = reblazer_to_elastic(line)
-            index_line = json.dumps({
-                "index" : {
-                    "_index" : "curieaccesslog",
-                    "_id" : recid
-                    }
-                })
+            ngxjson = json.loads(line)
+            timestamp = datetime.datetime.fromtimestamp(float(ngxjson["timestamp"])).isoformat()
+            eljson = reblazer_to_elastic(ngxjson, timestamp)
+            recid = eljson["requestid"]
+            index_line = json.dumps(gen_accesslog_index(recid))
             print (index_line, file=sys.stdout)
-            print (eljson, file=sys.stdout)
+            print (json.dumps(eljson), file=sys.stdout)
+
+            metric_doc = process_metrics(eljson, timestamp)
+            if metric_doc:
+                index_line = json.dumps(gen_metric_index(recid))
+                print (index_line, file=sys.stdout)
+                print (metric_doc, file=sys.stdout)
+        except TypeError:
+            raise
+
+        except AttributeError:
+            raise
 
         except Exception as err:
             print(err, file=sys.stderr)
--- curiefense/images/curielogger/elasticsearch/utils/fetch-convert-split-insert.sh
+++ curiefense/images/curielogger/elasticsearch/utils/fetch-convert-split-insert.sh
@@ -1,37 +1,42 @@
 #! /bin/bash
 
 ###########################################################################
-# this file downloads the last hour's 48th minute file from GS bucket     #
+# this file downloads the last hour's 36th minute file from GS bucket     #
 # of given planets. convert them into the ES format, plus adding          #
 # the _bulk index line (2 lines per document).                            #
 # split the output into a 20K lines each (50MB seems to be the threshold) #
 # insert the split files into Elasticsearch                               #
 ###########################################################################
 
+PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+HOME="/home/tzury/"
+BOTO_CONFIG="/home/tzury/.config/gcloud/legacy_credentials/tzury@reblaze.com/.boto"
 wd="/home/tzury/bqmigrate"
-planets="careblaze hzreblazeprod"
 
-## pick last hour's 48's minute. e.g. 20210131T0548
-timespamp=$(date -d "1 hour ago" "+%Y%m%dT%H48")
+## pick last hour's 36's minute. e.g. 20210131T0536
+timespamp=$(date -d "1 hour ago" "+%Y%m%dT%H36")
+logger fetch-convert-split-push $wd $planets $timestamp
+# cd $wd
 
-cd $wd
-
-for planet in $planets;
+for planet in "$@";
 do
-    gsutil cp "gs://rbz-logs-$planet/bqinsert/queue-in/compose/$timespamp-bq_load00" "bq-json/$planet$timespamp-bq_load00";
+    logger fetch-convert-split-push $planet
+    /snap/bin/gsutil cp "gs://rbz-logs-$planet/bqinsert/queue-in/compose/$timespamp-bq_load00" "$wd/bq-json/$planet$timespamp-bq_load00" >> /var/log/syslog;
 done
 
-for fi in bq-json/*-bq_load00;
+for fi in $wd/bq-json/*-bq_load00;
 do
-    echo $fi;
-    python3 utils/convert.py < $fi | split -l 20000 - "bq-json/$(basename $fi).split." -da 4;
+    logger fetch-convert-split-push $fi;
+    /usr/bin/python3 $wd/utils/convert.py < $fi | split -l 20000 - "$wd/bq-json/$(basename $fi).split." -da 4  >> /var/log/syslog;
 done
 
-for fi in bq-json/*.split.*;
+for fi in $wd/bq-json/*.split.*;
 do
-    curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/_bulk --data-binary "@$fi";
+    logger fetch-convert-split-push $fi
+    /usr/bin/curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/_bulk --data-binary "@$fi";
 done
 
 ## cleanup
-rm bq-json/*
+logger fetch-convert-split-push cleaning up
+rm $wd/bq-json/*
 
