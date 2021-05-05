f1ab74b11ad4199cebb567a03d10bd132584f7c8
tzury bar yochay <tzury@reblaze.com>
2021-05-05 11:38:19 +0300

    tgs are rendered by rust now

--- curiefense/curieproxy/lua/accesslog.lua
+++ curiefense/curieproxy/lua/accesslog.lua
@@ -13,11 +13,12 @@ function envoy_log_request(request_map)
   -- handle is userData which is not serilizable
   local request_handle = request_map.handle
   local entries = {
-    ["geo"]   = "geo",
+    ["geo"]     = "geo",
     ["headers"] = "headers",
     ["cookies"] = "cookies",
-    ["args"]  = "arguments",
-    ["attrs"]   = "attributes"
+    ["args"]    = "arguments",
+    ["attrs"]   = "attributes",
+    ["tags"]    = "tags"
   }
 
   local log_table = {}
@@ -26,20 +27,20 @@ function envoy_log_request(request_map)
     log_table[logname] = request_map[luaname]
   end
 
-  local tags = log_table.attributes.tags
+  -- local tags = log_table.attributes.tags
 
-  if tags then
-    local tagnames = {}
-    local n = 0
+  -- if tags then
+  --   local tagnames = {}
+  --   local n = 0
 
-    for name,v in pairs(tags) do
-      n = n + 1
-      tagnames[n] = name
-    end
+  --   for name,v in pairs(tags) do
+  --     n = n + 1
+  --     tagnames[n] = name
+  --   end
 
-    log_table.tags = tagnames
-    log_table.attributes.tags = nil
-  end
+  --   log_table.tags = tagnames
+  --   log_table.attributes.tags = nil
+  -- end
 
   log_table.blocked = log_table.attributes.blocked
   log_table.block_reason = log_table.attributes.block_reason
