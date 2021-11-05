e0c802e56be5733ac91774cf4a9434a3e3172233
Flavio Percoco <flavio@reblaze.com>
2021-11-05 06:08:18 +0000

    Remove deprecated outputs
    Signed-off-by: Flavio Percoco <flavio@reblaze.com>

--- curiefense/curielogger/pkg/initializer.go
+++ curiefense/curielogger/pkg/initializer.go
@@ -9,18 +9,13 @@ import (
 )
 
 const (
-	FLUENTD_ENABLED  = `CURIELOGGER_USES_FLUENTD`
-	STDOUT_ENABLED   = `CURIELOGGER_OUTPUTS_STDOUT_ENABLED`
-	BUCKET_ENABLED   = `CURIELOGGER_OUTPUTS_BUCKET_ENABLED`
-	LOGSTASH_ENABLED = `CURIELOGGER_OUTPUTS_LOGSTASH_ENABLED`
-	ES_ENABLED       = `CURIELOGGER_OUTPUTS_ELASTICSEARCH_ENABLED`
+	STDOUT_ENABLED = `CURIELOGGER_OUTPUTS_STDOUT_ENABLED`
+	BUCKET_ENABLED = `CURIELOGGER_OUTPUTS_BUCKET_ENABLED`
 )
 
 type OutputsConfig struct {
-	Elasticsearch outputs.ElasticsearchConfig `mapstructure:"elasticsearch,omitempty"`
-	Logstash      outputs.LogstashConfig      `mapstructure:"logstash,omitempty"`
-	Stdout        outputs.StdoutConfig        `mapstructure:"stdout,omitempty"`
-	Bucket        outputs.BucketConfig        `mapstructure:"bucket,omitempty"`
+	Stdout outputs.StdoutConfig `mapstructure:"stdout,omitempty"`
+	Bucket outputs.BucketConfig `mapstructure:"bucket,omitempty"`
 }
 
 func InitOutputs(v *viper.Viper, cfg Config) io.WriteCloser {
@@ -33,16 +28,5 @@ func InitOutputs(v *viper.Viper, cfg Config) io.WriteCloser {
 		output = append(output, outputs.NewBucket(v, cfg.Outputs.Bucket))
 	}
 
-	if v.GetBool(FLUENTD_ENABLED) {
-		output = append(output, outputs.NewFluentD(v))
-	}
-	if v.GetBool(LOGSTASH_ENABLED) || cfg.Outputs.Logstash.Enabled {
-		output = append(output, outputs.NewLogstash(v, cfg.Outputs.Logstash))
-	}
-	if v.GetBool(ES_ENABLED) || cfg.Outputs.Elasticsearch.Enabled {
-		if es := outputs.NewElasticSearch(v, cfg.Outputs.Elasticsearch); es != nil {
-			output = append(output, es)
-		}
-	}
 	return outputs.NewTee(output)
 }
