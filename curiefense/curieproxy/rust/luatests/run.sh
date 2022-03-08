6cb25f150c658b1932fbecdc5b86fdeed69d0ba9
Simon Marechal <bartavelle@gmail.com>
2022-03-08 11:13:38 +0100

    New content filter rules + gowaftest
    Signed-off-by: Simon Marechal <bartavelle@gmail.com>

--- curiefense/curieproxy/rust/luatests/run.sh
+++ curiefense/curieproxy/rust/luatests/run.sh
@@ -2,4 +2,4 @@
 curiefense/ui/src/components/EntriesRelationList.vue#!/bin/sh
 redis-server --loglevel notice &
 export REDIS_HOST=127.0.0.1
-luajit /home/builder/test.lua
+luajit /home/builder/test.lua "$1"
