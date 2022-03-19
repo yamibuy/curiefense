77e2d3be193ffe2bb37648e028e1f4bdbf5d8d4a
Tzury Bar Yochay <tzury@reblaze.com>
2022-03-19 19:23:52 +0200

    not clear how show lines were added
    ```
    #!/bin/bash
    curiefense/ui/src/components/EntriesRelationList.vue
    ```

--- curiefense/curieproxy/rust/luatests/run.sh
+++ curiefense/curieproxy/rust/luatests/run.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-curiefense/ui/src/components/EntriesRelationList.vue#!/bin/sh
+#!/bin/sh
 redis-server --loglevel notice &
 export REDIS_HOST=127.0.0.1
 luajit /home/builder/test.lua "$1"
